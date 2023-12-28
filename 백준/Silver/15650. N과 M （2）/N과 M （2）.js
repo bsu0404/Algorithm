63afbde6fd05ef7a6020ad1f859629446da8a4c1
변성은 <bsu0404@naver.com>
2023-12-28 15:09:39 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+
+for (let i = 0; i < input.length; i++) {
+  input[i] = parseInt(input[i]);
+}
+
+function dfs(arr) {
+  if (arr.length == input[1]) {
+    console.log(arr.join(" "));
+  } 
+    for (let i = 1; i <= input[0]; i++) {
+      if (arr.indexOf(i) != -1 || (arr[arr.length-1]>i)) { //중복이면 , 오름차순이 아니면
+        continue;
+      }
+      dfs([...arr, i]); 
+    }
+  
+}
+dfs([]);
