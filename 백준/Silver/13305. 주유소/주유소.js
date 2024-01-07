0dac9f3c185f0d36a06fa7f6997ed2e667875b23
변성은 <bsu0404@naver.com>
2024-01-07 18:59:38 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 45712 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,20 +3,19 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let N = parseInt(input[0]);
-let len = input[1].split(" ").map(Number);
-let costs = input[2].split(" ").map(Number);
-let sum = 0;
-let min = costs[0];
-let current_len = 0;
+let len = input[1].split(" ").map(BigInt);
+let costs = input[2].split(" ").map(BigInt);
+let sum = BigInt(0);
+let min = BigInt(costs[0]);
+let current_len = BigInt(0);
 
 for (let i = 0; i < costs.length; i++) {
   let cost = costs[i];
-  
-  if (cost < min || i == N - 1) {
+    if (cost < min || i == N - 1) {
     sum = sum + current_len * min;
     min = cost;
-    current_len = 0;
+    current_len = BigInt(0);
   }
-  current_len = current_len + len[i];
+  if (i != N - 1) current_len = current_len + BigInt(len[i]);
 }
-console.log(sum);
+console.log(sum.toString());
