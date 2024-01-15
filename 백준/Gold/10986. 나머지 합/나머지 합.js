f9066ff7c9897817b2ae74e07078e61fc6db1c62
변성은 <bsu0404@naver.com>
2024-01-16 05:35:24 +0900

    [Gold III] Title: 나머지 합, Time: 640 ms, Memory: 153916 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [N, M] = input[0].split(" ").map(Number);
+let array = input[1].split(" ").map(Number);
+
+let quotient = Array(M + 1).fill(0);
+quotient[0] = 1;
+let sum = [0];
+
+for (let i = 1; i <= N; i++) {
+  sum.push(sum[i - 1] + array[i - 1]);
+  quotient[sum[i] % M]++;
+}
+let result = 0;
+for (let i = 0; i < M; i++) {
+  result += (quotient[i] * (quotient[i] - 1)) / 2;
+}
+console.log(result);
