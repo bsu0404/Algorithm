ac6390b80589585e5ea66815069efba371f07c62
변성은 <bsu0404@naver.com>
2024-05-02 14:20:09 +0900

    [Gold II] Title: 트리의 지름, Time: 5220 ms, Memory: 91836 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+# function
+
+def dfs(index, cost):
+    global maxIndex, maxCost
+    visited[index] = 1
+    visitArr = []
+
+    for node in tree[index]:
+        if visited[node[0]] == 0:
+            visitArr.append(node)
+    # 말단 노드
+    if len(visitArr) == 0:  
+        if maxCost < cost:
+            maxCost = cost
+            maxIndex = index
+    # 이어진 노드로 이동
+    for node in visitArr:
+        visited[node[0]] = 1
+        dfs(node[0], cost + node[1])
+
+
+# main
+
+N = int(input())
+tree = [[] for _ in range(N + 1)]
+
+
+for _ in range(N):
+    arr = list(map(int, input().split()))
+    v1 = arr[0]
+    i = 1
+    while arr[i] != -1:
+        v2, w = arr[i], arr[i+1]
+        tree[v1].append([v2, w])
+        tree[v2].append([v1, w])
+        i += 2
+
+
+visited = [0] * (N + 1)
+maxCost = 0
+maxIndex = -1
+dfs(1,0)
+
+maxCost = 0
+visited = [0] * (N + 1)
+dfs(maxIndex, 0)
+
+print(maxCost)
