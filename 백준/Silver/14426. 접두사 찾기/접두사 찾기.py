b9bdcda9d3674a10e59f1d2d5f807be81f3b7b07
변성은 <bsu0404@naver.com>
2024-06-24 11:40:35 +0900

    [Silver I] Title: 접두사 찾기, Time: 1740 ms, Memory: 44572 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
@@ -3,22 +3,22 @@ N, M = map(int, input().split())
 arr1 = [input() for _ in range(N)]
 arr2 = [input() for _ in range(M)]
 
-root = [-1, False, {}]
-# value 없어도 되지 않나?
+root = {}
 for word in arr1:
     p = root
     for c in word:
-        if c not in p[2]:
-            p[2][c] = [c,False, {}]
-        p = p[2][c]
+        if c not in p:
+            p[c] = {}
+        p = p[c]
     p[1] = True
+
 ans = 0
 for word in arr2:
     p = root
     flag = True
     for c in word:
-        if c in p[2]:
-            p = p[2][c]
+        if c in p:
+            p = p[c]
         else:
             flag = False
     if flag == True:    
