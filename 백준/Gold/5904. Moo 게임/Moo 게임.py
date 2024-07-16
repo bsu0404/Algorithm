080df1c7daa646d359a218c575085c2943780661
변성은 <bsu0404@naver.com>
2024-07-16 12:03:39 +0900

    [Gold V] Title: Moo 게임, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.py
+++ 백준/Gold/5904. Moo 게임/Moo 게임.py
@@ -9,36 +9,39 @@ k = 0
 while N > lenArr[k] : 
     k += 1
 
-s = 1
 while True:
-    if k == 0 :
-        if s == 0 :
-            print('m')
-        else :
-            print('o')
+    
+
+    if k == 0 and N == 1:
+        print('m')
+        break
+    elif k == 0:
+        print('o')
         break
 
-    # 각 구역의 사이즈 결정자 = k
-    size1 = lenArr[k-1]
+
+    size1 = lenArr[k - 1]
     size2 = k + 3
-    part1 = s
-    part2 = part1 + size1
-    part3 = part2 + size2
-    
-    if N in (part1, part2, part3) :
+
+    part1 = 1
+    part2 = part1 + size1 
+    part3 = part2 + size2 
+
+
+    if N in (part1, part2, part3):
         print('m')
         break
 
-    # part1 : 첫 번째 구역
-    if N < part2:
-        s = part1
-    # part2 : 두 번째 구역
+    # part1 : 앞
+    if part1 <= N < part2:
+        k -= 1 
+
+    # part2: 가운데
     elif part2 <= N < part3:
         print('o')
         break
-    # part3 : 세 번째 구역
-    else: # part3 <= N
-        s = part3
 
-    k -= 1
-
+    # part3: 뒤
+    else:
+        N = N - size1 - size2
+        k -= 1
