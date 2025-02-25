3c1e3442d623541a1116171a460984489c98fa1a
변성은 <bsu0404@naver.com>
2025-02-25 15:47:42 +0900

    [Gold IV] Title: LCS 2, Time: 892 ms, Memory: 420756 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -4,21 +4,20 @@ len1= len(str1)
 len2 = len(str2)
 
 
-mtx = [[[0,""] for _ in range(len1+1)] for _ in range(len2+1)]
+mtx = [["" for _ in range(len1+1)] for _ in range(len2+1)]
 
 for i in range(1, len2 + 1):
     for j in range(1, len1 + 1):
         if (str2[i-1]==str1[j-1]):
-            mtx[i][j][0] = mtx[i-1][j-1][0] + 1
-            mtx[i][j][1] = mtx[i-1][j-1][1] + str1[j-1]
+            mtx[i][j] = mtx[i-1][j-1] + str1[j-1]
         else:
-            mtx[i][j][0] = max(mtx[i-1][j][0],mtx[i][j-1][0])
-            if mtx[i-1][j][0] > mtx[i][j-1][0]:
-                mtx[i][j][1] = mtx[i-1][j][1]
+            mtx[i][j] = max(mtx[i-1][j],mtx[i][j-1])
+            if len(mtx[i-1][j]) > len(mtx[i][j-1]):
+                mtx[i][j] = mtx[i-1][j]
             else:
-                mtx[i][j][1] = mtx[i][j-1][1] 
-                
-lcs = mtx[len2][len1][0]
-print(lcs)
-if lcs != 0:
-    print(mtx[len2][len1][1])
+                mtx[i][j] = mtx[i][j-1]
+
+lcs = mtx[len2][len1]
+print(len(lcs))
+if lcs:
+    print(lcs)
